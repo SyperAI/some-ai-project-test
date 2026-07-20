c6554fc629c6c25a0ffde02e37abf40ffda95028
StarMan <alexkom041206@gmail.com>
2026-07-20 17:47:24 +0200

    Updated install.bat
    Took 1 minute

--- install.bat
+++ install.bat
@@ -1,6 +1,12 @@
 @echo off
 
-if not exists "%~dp0venv\" python -m venv venv
+if not exists "%~dp0venv\" (
+    echo "venv not found, creating..."
+    python -m venv venv
+) else (
+    echo "Found venv"
+)
+
 
 "./venv/Scripts/python.exe" -m pip install --upgrade pip
 "./venv/Scripts/pip.exe" install -r requirements.txt
