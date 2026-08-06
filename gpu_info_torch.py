d6c2306d56ea7f294821c0b899e941e9b9b0ea45
StarMan <alexkom041206@gmail.com>
2026-08-06 16:50:40 +0200

    Improved work of gpu_info_torch.py
    Took 23 minutes

--- gpu_info_torch.py
+++ gpu_info_torch.py
@@ -1,5 +1,8 @@
 import json
 import logging
+import sys
+from pathlib import Path
+
 import torch
 
 
@@ -44,11 +47,48 @@ def get_gpu_name(device_id: int = 0) -> str | None:
         return None
 
 
+def device_id_poll() -> int:
+    num_devices = torch.cuda.device_count()
+    print(f"Found {num_devices} CUDA device{'s' if num_devices > 1 else ''}")
+    print("-" * 40)
+
+    for device_id in range(num_devices):
+        name = torch.cuda.get_device_name(device_id)
+        print(f"{device_id}. {name}")
+
+    device_id = input(f"Please enter device id (0~{num_devices - 1}): ")
+    try:
+        device_id = int(device_id)
+    except:
+        return device_id_poll()
+
+    if device_id < 0 or device_id > num_devices - 1:
+        return device_id_poll()
+
+    return device_id
+
+
 if __name__ == '__main__':
+    if not torch.cuda.is_available(): sys.exit(0)
+
+    info_file = Path('.gpuinfo')
+
+    if info_file.exists():
+        gpu_pre_info = json.load(info_file.open('r'))
+        if 'device_id' in gpu_pre_info.keys() and gpu_pre_info['device_id'] is not None:
+            did = gpu_pre_info['device_id']
+            print(f"Found old data from device {did}. Old data was deleted now, re-start app if something went wrong with this data.")
+            info_file.unlink(missing_ok=True)
+
+        else: did = device_id_poll()
+    else:
+        did = device_id_poll()
+
     gpu_info = {
-        "vram": get_vram(0),
-        "compute_cap": get_compute_cap(0),
-        "gpu_name": get_gpu_name(0),
+        "vram": get_vram(did),
+        "compute_cap": get_compute_cap(did),
+        "gpu_name": get_gpu_name(did),
+        "device_id": did
     }
 
-    json.dump(gpu_info, open('.gpuinfo', 'w'))
+    json.dump(gpu_info, info_file.open('w'))
